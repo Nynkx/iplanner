8c6f2e170f184d383504d14028e5acb1eb8d9eae
unknown <conghuy649@gmail.com>
2023-03-24 15:56:19 +0700

    initial commit

---
+++ android/app/src/main/java/com/neovation/iplanner/MainActivity.java
@@ -0,0 +1,5 @@
+package com.neovation.iplanner;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.neovation.iplanner',
+  appName: 'iPlanner',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
